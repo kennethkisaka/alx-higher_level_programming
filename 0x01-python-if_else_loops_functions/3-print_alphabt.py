81c58c27d699c5a1154ca7733895e6d7d8c7bbe8
Kenneth Kisaka Kawwa <122867145+kennethkisaka@users.noreply.github.com>
2024-03-12 18:30:05 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == ord("e") or i == ord("q"):
-        continue
-    print("{}".format(chr(i)), end="")
+result = ''.join(chr(i) for i in range(97, 123) if i not in (ord('e'), ord('q')))
