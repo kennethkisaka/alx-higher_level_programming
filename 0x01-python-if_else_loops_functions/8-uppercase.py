7ccb5ef03756b80a5b698ef54a3d2a3c66349c84
Kenneth Kisaka Kawwa <122867145+kennethkisaka@users.noreply.github.com>
2024-03-13 00:50:00 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in list(str):
         if ord(i) <= 122 and ord(i) >= 97:
-            print(chr(ord(i) - 32), end="")
+            print("{:s}".format(chr(ord(i) - 32)), end="")
         else:
-            print(i, end="")
+            print("{:s}".format(i), end="")
