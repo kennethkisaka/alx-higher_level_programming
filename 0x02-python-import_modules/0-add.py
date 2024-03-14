b782caadad72c0610e55758efe5044f581d066b9
Kenneth Kisaka Kawwa <122867145+kennethkisaka@users.noreply.github.com>
2024-03-14 21:58:56 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,4 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 result_add = add(a, b)
-print("{a:d} + {b:d} = {result_add:d}".format(a, b, result_add))
+print(f"{a} + {b} = {result_add}")
